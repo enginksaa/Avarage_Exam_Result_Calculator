9e99c0c5d20c3f15bbb260419caa0f0a71671b69
enginksaa <90872388+enginksaa@users.noreply.github.com>
2023-05-09 20:24:56 +0300

    AvarageExamResults.java

--- AvarageExamResults.java
+++ AvarageExamResults.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class AvarageExamResults {
     public static void main(String[] args) {
-        // değişkenler
+       
         int maths;
         int phys;
         int turkish;
@@ -28,8 +28,7 @@ public class AvarageExamResults {
         double result = (maths + phys + turkish + history + music);
         double finalresult = result / 5;
         System.out.println(" Avarage Result : " + finalresult);
-        // buradaki + işareti sonrasında gelen result Int yada double bile olsa string
-        // gibi metne bağlar öyle cıktı verir
+      
         boolean passFailed = (finalresult >= 60);
         String strPassFailed = (passFailed) ? "Sinifi Gecti" : "Sinifta Kaldi";
         System.out.println(strPassFailed);
